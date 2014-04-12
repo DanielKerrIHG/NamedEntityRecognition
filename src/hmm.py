813fe01a6a031c840aee2bd72184e713a525c25d
Zankoku Okuno <zankoku.okuno@gmail.com>
2014-04-11 23:02:13 -0400

    viterbi algorithm

--- src/hmm.py
+++ src/hmm.py
@@ -46,3 +46,26 @@ def normalize(init, stt, emm):
             for pos in row:
                 emm[st][pos] /= total
     return init, stt, emm
+
+def viterbi(sentence, init, stt, emm):
+    V = [{chunk: init[chunk] * emm[chunk][sentence[0][1]] for chunk in allChunk}]
+    path = {chunk: [chunk] for chunk in allChunk}
+
+    def step_prob(chunk1, chunk2, t):
+        return V[t-1][chunk1] * stt[chunk1][chunk2] * emm[chunk2][sentence[t][1]]
+
+    for t in range(1, len(sentence)):
+        V.append(dict())
+        newpath = dict()
+
+        for chunk2 in allChunk:
+            p, likelyChunk = max((step_prob(chunk1, chunk2, t), chunk1) for chunk1 in allChunk)
+            V[t][chunk2] = p
+            newpath[chunk2] = path[likelyChunk] + [chunk2]
+        path = newpath
+
+    assert t == len(sentence) - 1
+    p, likely = max((V[t][chunk], chunk) for chunk in allChunk)
+    return p, path[likely]
+
+

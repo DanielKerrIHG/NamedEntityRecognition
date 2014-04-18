08edc07d65f9db918b9221bd6394ec9d35cfef04
Zankoku Okuno <zankoku.okuno@gmail.com>
2014-04-18 18:04:31 -0400

    reactivated the prlg predictor

--- src/prlg.py
+++ src/prlg.py
@@ -9,7 +9,7 @@ class PRLG(Markov):
         self.emission = {(chunk1, chunk2): {pos: 0 for pos in poss} for chunk1 in chunks for chunk2 in chunks}
         self.emission.update({(chunk1, None): {pos: 0 for pos in poss} for chunk1 in chunks})
 
-    def _trainInitial_from_back(self, data):
+    def _trainInitial(self, data):
         # count the chunk value on the first word of every sentence
         for sentence in data:
             self.initial[sentence[-1].chunk] += 1
@@ -18,14 +18,6 @@ class PRLG(Markov):
         for st in self.initial:
             self.initial[st] /= total
     
-    def _trainInitial(self, data):
-        for sentence in data:
-            if len(sentence) == 1:
-                chunk1, chunk2 = sentence[0].chunk, None
-            else:
-                chunk1, chunk2 = sentence[0].chunk, sentence[1].chunk
-            self.initial[(chunk1, chunk2)] += 1
-
     def _trainEmission(self, data):
         # count the associations of a chunk bigram with an observable
         for sentence in data:
@@ -42,71 +34,6 @@ class PRLG(Markov):
 
 
     def predict(self, sentence):
-        V = [{chunk: sum(self.initial[(chunk1, chunk2)] * self.emission[(chunk1, chunk2)][sentence[0].pos] for chunk2 in self.chunks) for chunk1 in self.chunks}]
-        path = {chunk: [chunk] for chunk in self.chunks}
-
-        def step_prob(chunk1, chunk2, t):
-            return V[t-1][chunk1] * self.transition[chunk1][chunk2] * self.emission[chunk2][sentence[t].pos]
-
-        t = 0
-        for t in range(1, len(sentence) - 1):
-            V.append(dict())
-            newpath = dict()
-
-            for chunk2 in self.chunks:
-                p, likelyChunk = max((step_prob(chunk1, chunk2, t), chunk1) for chunk1 in self.chunks)
-                V[t][chunk2] = p
-                newpath[chunk2] = path[likelyChunk] + [chunk2]
-            path = newpath
-
-        try:
-            assert t == len(sentence) - 1
-        except:
-            raise Exception(sentence)
-        p, likely = max((V[t][chunk], chunk) for chunk in self.chunks)
-
-        for i, word in enumerate(sentence):
-            word.predicted = path[likely][i]
-        return sentence
-
-    def predict_fails(self, sentence):
-        V = [{'B': 1, 'I': 0, 'O': 0}]
-        path = {chunk: [chunk] for chunk in self.chunks}
-
-        def step_prob(chunk1, chunk2, t):
-            return V[t-1][chunk1] * self.transition[chunk1][chunk2] * self.emission[(chunk1, chunk2)][sentence[t-1].pos]
-
-        t = 0
-        for t in range(1, len(sentence)-1):
-            V.append(dict())
-            newpath = dict()
-
-            for chunk2 in self.chunks:
-                p, likelyChunk = max((step_prob(chunk1, chunk2, t), chunk1) for chunk1 in self.chunks)
-                V[t][chunk2] = p
-                newpath[chunk2] = path[likelyChunk] + [chunk2]
-            path = newpath
-        
-        t += 1
-        V.append(dict())
-        newpath = dict()
-        p, likelyChunk = max((V[t-1][chunk1] * self.emission[(chunk1, None)][sentence[t-1].pos], chunk1) for chunk1 in self.chunks)
-        V[t][chunk2] = p
-        newpath[chunk2] = path[likelyChunk] + [chunk2]
-        path = newpath
-
-
-        try:
-            assert t == len(sentence) - 1
-        except:
-            raise Exception(sentence)
-        p, likely = max((V[t-1][chunk], chunk) for chunk in self.chunks)
-
-        for i, word in enumerate(sentence):
-            word.predicted = path[likely][i]
-        return sentence
-
-    def predict_from_back(self, sentence):
         V = [dict() for x in range(0, len(sentence))]
         V[-1] = {chunk: self.initial[chunk] * self.emission[(chunk, None)][sentence[-1].pos] for chunk in self.chunks}
         path = {chunk: [chunk] for chunk in self.chunks}
--- test.py
+++ test.py
@@ -4,6 +4,7 @@ loadData = importDetailed
 train = loadData('conll2000/train.txt')
 test = loadData('conll2000/test.txt')
 test2 = loadData('conll2000/test.txt')
+test3 = loadData('conll2000/test.txt')
 combined = loadData('conll2000/combined.txt')
 
 allPos = set(word.pos for sentence in train + test for word in sentence)
@@ -16,16 +17,18 @@ sliceData(combined, numberOfSlices, trainSlices, testSlices)
 
 
 from src.hmm import *
+from src.prlg import *
 from src.pllg import *
 
 print("training...")
 predictor = Markov(allPos, allChunks).train(train)
-predictor2 = PLLG(allPos, allChunks).train(train)
+predictor2 = PRLG(allPos, allChunks).train(train)
+predictor3 = PLLG(allPos, allChunks).train(train)
 
 
 # i = 0
 # it = predictor.predict(test[i])
-# it2 = predictor2.predict(test2[i])
+# it2 = predictor3.predict(test3[i])
 # assert len(it) == len(it2)
 # for i in range(0, len(it)):
 # 	print(it[i].predicted, it2[i].predicted)
@@ -38,6 +41,9 @@ for sentence in test:
 predictions2 = []
 for sentence in test2:
 	predictions2.append(predictor2.predict(sentence))
+predictions3 = []
+for sentence in test3:
+	predictions3.append(predictor3.predict(sentence))
 
 
 # count = 0
@@ -49,14 +55,8 @@ for sentence in test2:
 print("calculating weighted f1...")
 print(f1_weighted(predictions, allChunks))
 print(f1_weighted(predictions2, allChunks))
+print(f1_weighted(predictions3, allChunks))
 
-print("calculating micro average f1...")
-print(f1_micro(predictions, allChunks))
-print(f1_micro(predictions2, allChunks))
-
-print("calculating macro average f1...")
-print(f1_macro(predictions, allChunks))
-print(f1_macro(predictions2, allChunks))
 
 """
 print('========================================')
